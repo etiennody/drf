13d30bc955bbe0f3c97882f239dc6b696771ebc3
etiennody <etiennody@gmail.com>
2021-03-14 21:28:52 +0100

    added dj-rest-auth configuration

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("", TestView.as_view(), name="test"),
     path("api/token/", obtain_auth_token, name="obtain-token"),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
 ]
