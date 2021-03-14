ff0950de773f4e185ff65079005cf13984e4b5ff
etiennody <etiennody@gmail.com>
2021-03-14 10:30:28 +0100

    added first test view function

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def test_view(request):
+    data = {"name": "John", "age": 23}
+    return JsonResponse(data)
