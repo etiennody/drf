a27276a6b243994814d5402a884323992bf05449
etiennody <etiennody@gmail.com>
2021-03-14 21:57:47 +0100

    updated listing post view

--- core/views.py
+++ core/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+
+# from rest_framework.views import APIView
+from rest_framework import generics, mixins
+
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-class TestView(APIView):
-    permission_classes = (IsAuthenticated,)
+class PostView(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        queryset = Post.objects.all()
-        serializer = PostSerializer(queryset, many=True)
-        return Response(serializer.data)
+        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+
+# class TestView(APIView):
+#     permission_classes = (IsAuthenticated,)
+
+#     def get(self, request, *args, **kwargs):
+#         queryset = Post.objects.all()
+#         serializer = PostSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,4 +1,4 @@
-from core.views import TestView
+from core.views import PostView
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("", TestView.as_view(), name="test"),
+    path("", PostView.as_view(), name="post"),
     path("api/token/", obtain_auth_token, name="obtain-token"),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
 ]
